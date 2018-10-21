e2b082576c2c4eeb4ea7179128dc86eca2a46513
MoeMamdouh <mohamed.mamdouh.do@gmail.com>
2018-10-21 16:20:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,87 +1,91 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
-import {
-  View,
-  Dimensions,
-  NativeMethodsMixin,
-} from 'react-native'
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+import { View, Dimensions, NativeMethodsMixin } from 'react-native';
 
-class InView extends Component{
-  constructor(props){
-    super(props)
+class InView extends Component {
+  constructor(props) {
+    super(props);
 
-    this.state ={
+    this.state = {
       rectTop: 0,
-      rectBottom: 0
-    }
+      rectBottom: 0,
+    };
   }
 
   onLayout() {
     if (this.props.active) {
-      this.startWatching()
+      this.startWatching();
     }
   }
 
   componentWillUnmount() {
-    this.stopWatching()
+    this.stopWatching();
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.active) {
-      this.lastValue = null
-      this.startWatching()
+      this.lastValue = null;
+      this.startWatching();
     } else {
-      this.stopWatching()
+      this.stopWatching();
     }
   }
 
   startWatching() {
-    if (this.interval) { return }
-    this.interval = setInterval(event => this.check(), this.props.delay)
+    if (this.interval) {
+      return;
+    }
+    this.interval = setInterval(event => this.check(), this.props.delay);
   }
 
   stopWatching() {
-    this.interval = clearInterval(this.interval)
+    this.interval = clearInterval(this.interval);
   }
   check() {
-    let win = Dimensions.get('window')
-    let el = this.refs.myview
+    let win = Dimensions.get('window');
+    let el = this.refs.myview;
     let rect = el.measureInWindow((x, y, width, height) => {
       this.setState({
         rectTop: y,
         rectBottom: y + height,
         rectWidth: x + width,
-      })
-    })
-    let isVisible = (
-      this.state.rectBottom != 0 && this.state.rectTop >= 0 && this.state.rectBottom <= win.height &&
-      this.state.rectWidth > 0 && this.state.rectWidth <= win.width
-    )
+      });
+    });
+    let isVisible =
+      this.state.rectBottom != 0 &&
+      this.state.rectTop >= 0 &&
+      this.state.rectBottom <= win.height &&
+      this.state.rectWidth > 0 &&
+      this.state.rectWidth <= win.width;
 
     // notify the parent when the value changes
     if (this.lastValue !== isVisible) {
-      this.lastValue = isVisible
-      this.props.onChange(isVisible)
+      this.lastValue = isVisible;
+      this.props.onChange(isVisible);
     }
   }
 
-  render(){
+  render() {
     return (
-      <View ref="myview" {...this.props} onLayout={event => this.onLayout(event)}>
+      <View
+        ref="myview"
+        {...this.props}
+        onLayout={event => this.onLayout(event)}
+      >
         {this.props.children}
       </View>
-    )
+    );
   }
 }
 
 InView.defaultProps = {
   active: true,
   delay: 100,
-}
+};
 InView.propTypes = {
-  onChange: PropTypes.func.isRequired,
-  active: PropTypes.bool,
-  delay: PropTypes.number
-}
+  onChange: propTypes.func.isRequired,
+  active: propTypes.bool,
+  delay: propTypes.number,
+};
 
-module.exports = InView
+module.exports = InView;
